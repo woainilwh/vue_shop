ffbe8e4fd52ea241e135b2a06a5bb901a90fae07
刘恒 <302862951@qq.com>
2021-03-25 15:02:17 +0800

    完成商品功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,7 +4,7 @@ import { Button,Message,Form,FormItem,Input,
         MenuItemGroup,MenuItem,Breadcrumb,BreadcrumbItem,
         Card,Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,
         Dialog,MessageBox,Tag,Tree,Select,Option,Cascader,Alert,
-        Tabs,TabPane,
+        Tabs,TabPane,Steps,Step,Checkbox,CheckboxGroup,Upload
       } from 'element-ui'
 
 Vue.use(Button)
@@ -38,6 +38,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Step)
+Vue.use(Steps)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm =MessageBox.confirm
