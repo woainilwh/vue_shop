793ea2d787fc3374f02e25e9837c92460f951036
刘恒 <302862951@qq.com>
2021-03-30 14:10:18 +0800

    生成dist文件项目基本完成

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import './plugins/element.js'
+// import './plugins/element.js'
 import './assets/global.css'
 import axios from 'axios'
 import TreeTable from "vue-table-with-tree-grid"
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 // 导入富文本编辑器对应的样式
-import 'quill/dist/quill.core.css' 
-import 'quill/dist/quill.snow.css' 
-import 'quill/dist/quill.bubble.css' 
+
+// 导入nprogress包对应的js和css
+import Nprogress from "nprogress"
 
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+// 在request拦截器中展示进度条 Nprogress.start()
 // 在请求挂载之前设置请求拦截器
 axios.interceptors.request.use(config =>{
   // console.log(config);
+  Nprogress.start()
   // 给config里的headers属性添加一个值
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 最后必须return config  
   return config 
+})
+// 在response拦截器中 隐藏进度条 Nprogress.done()
+axios.interceptors.response.use(config => {
+  Nprogress.done()
+  return config
 })
 Vue.prototype.$http = axios
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from "../components/user/User.vue"
-import Rights from "../components/power/Rights.vue"
-import Roles from "../components/power/Roles.vue"
-import Cate from "../components/goods/Cate.vue"
-import Params from "../components/goods/Params.vue"
-import List from "../components/goods/List.vue"
-import Add from "../components/goods/Add.vue"
-import Order from "../components/order/Order.vue"
-import Report from "../components/report/Report.vue"
+const Login= () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
+const Home= () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
+const Welcome= () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
+
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+
+const Users= () => import(/* webpackChunkName: "users_rights_rples" */ '../components/user/User.vue')
+const Rights= () => import(/* webpackChunkName: "users_rights_rples" */ '../components/power/Rights.vue')
+const Roles= () => import(/* webpackChunkName: "users_rights_rples" */ '../components/power/Roles.vue')
+// import Users from "../components/user/User.vue"
+// import Rights from "../components/power/Rights.vue"
+// import Roles from "../components/power/Roles.vue"
+
+const Cate= () => import(/* webpackChunkName: "cate_params" */ '../components/goods/Cate.vue')
+const Params= () => import(/* webpackChunkName: "cate_params" */ '../components/goods/Params.vue')
+// import Cate from "../components/goods/Cate.vue"
+// import Params from "../components/goods/Params.vue"
+
+const List= () => import(/* webpackChunkName: "list_add" */ '../components/goods/List.vue')
+const Add= () => import(/* webpackChunkName: "list_add" */ '../components/goods/Add.vue')
+// import List from "../components/goods/List.vue"
+// import Add from "../components/goods/Add.vue"
+
+const Order= () => import(/* webpackChunkName: "order_report" */ '../components/order/Order.vue')
+const Report= () => import(/* webpackChunkName: "order_report" */ '../components/report/Report.vue')
+// import Order from "../components/order/Order.vue"
+// import Report from "../components/report/Report.vue"
 Vue.use(Router)
 
 
